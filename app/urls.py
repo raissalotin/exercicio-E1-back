516d85136b5d499ab89154308923630f55758402
raissalotin <rahlotin@gmail.com>
2024-07-09 15:15:13 -0300

    criação api acessorio

--- app/urls.py
+++ app/urls.py
@@ -9,10 +9,11 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, 
+from core.views import UserViewSet
+from core.views import AcessorioViewSet
 
 router = DefaultRouter()
-
+router.register(r"acessorios", AcessorioViewSet)
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 
 urlpatterns = [

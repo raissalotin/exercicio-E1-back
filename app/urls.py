76cc2f404e84e72199a71c72838ffdb000befa39
raissalotin <rahlotin@gmail.com>
2024-07-12 14:39:16 -0300

    criação API categoria

--- app/urls.py
+++ app/urls.py
@@ -11,10 +11,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import UserViewSet
 from core.views import AcessorioViewSet
+from core.views import CategoriaViewSet
 
 router = DefaultRouter()
-router.register(r"acessorios", AcessorioViewSet)
+
 router.register(r"usuarios", UserViewSet, basename="usuarios")
+router.register(r"categorias", CategoriaViewSet, )
+router.register(r"acessorios", AcessorioViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),

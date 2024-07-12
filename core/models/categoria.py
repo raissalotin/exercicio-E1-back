83af583fa048fb4c2be0b6a30d82af9352bc0459
raissalotin <rahlotin@gmail.com>
2024-07-12 15:11:58 -0300

    inclusao de dados model categoria

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -5,5 +5,5 @@ class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
 
-def __str__(self):
-    return self.descricao
+    def __str__(self):
+        return self.descricao
